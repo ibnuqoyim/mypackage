3c6d3d764054822f16758dceb62d2ebb34b9c88c
Muhamad Ibnu Qoyim <muhamadibnu9@gmail.com>
2024-07-02 16:09:47 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ibnuqoyim/mypackage
+
+go 1.21.0
---
+++ mypackage.go
@@ -0,0 +1,9 @@
+// mypackage/mypackage.go
+package mypackage
+
+import "fmt"
+
+// PublicFunction adalah contoh fungsi yang dapat diakses dari luar package.
+func PublicFunction() {
+	fmt.Println("Ini adalah fungsi dari mypackage!")
+}
